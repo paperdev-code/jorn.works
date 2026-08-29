e5140e73605ab79d4d400c3872da38996fa3ec85
jorn <git@jorn.software>
2026-08-29 22:27:02 +0200

    add simulated terminal
    this looks bad on mobile

--- src/index.js
+++ src/index.js
@@ -21,3 +21,92 @@ window.addEventListener('load', async function() {
   }
   requestAnimationFrame(onFrame);
 });
+
+class Mutex {
+  constructor() {
+    this.locked = false;
+    this.priorityQueue = [];
+  }
+
+  async lock() {
+    while (this.locked) await new Promise((res) => {
+      this.priorityQueue.push(res);
+    });
+    this.locked = true;
+  }
+
+  unlock() {
+    this.locked = false;
+    if (this.priorityQueue.length > 0) {
+      const res = this.priorityQueue.shift();
+      res();
+    }
+  }
+};
+
+(function() {
+  const stdin = document.getElementById('stdin');
+  const stdout = document.getElementById('stdout');
+  const prefix = document.getElementById('prefix');
+
+  async function writeStdin(text) {
+    console.log(text);
+    for (let idx = 0; idx < text.length; ++idx) {
+      stdin.innerText += text[idx];
+      await sleep(40);
+    }
+  }
+
+  function dupeElementIntoStdout(elt) {
+    const stdin_line = document.createElement('span');
+    stdin_line.innerText = `${prefix.innerText}${stdin.innerText}`;
+    const copy = elt.cloneNode(true);
+    copy.id = '';
+    stdout.insertBefore(stdin_line, stdout.lastElementChild);
+    stdout.insertBefore(copy, stdout.lastElementChild);
+  }
+  
+  const actions = (function() {
+    const result = new Map();
+    document.querySelectorAll('#terminal-actions > div').forEach(function (elt) {
+      result.set(elt.id, {
+        stdinText: elt.dataset.stdin,
+        stdoutElt: elt,
+      });
+    });
+    return result;
+  })()
+
+  const terminal_mutex = new Mutex();
+
+  async function sleep(ms) {
+    await new Promise(res => setTimeout(res, ms));
+  }
+
+  async function performAction(action) {
+    await terminal_mutex.lock();
+    await writeStdin(action.stdinText);
+    dupeElementIntoStdout(action.stdoutElt);
+    stdin.innerText = '';
+    stdout.scrollTop = stdout.scrollHeight;
+    terminal_mutex.unlock();
+  }
+
+  function performActionFromUrlIfAvailable(url) {
+    const parsed = new URL(url);
+    const action = actions.get(parsed.hash.substring(1));
+    action && performAction(action);
+  }
+
+  window.addEventListener('hashchange', function(evt) {
+    performActionFromUrlIfAvailable(evt.newURL);
+  });
+
+  window.addEventListener('load', async function () {
+    performActionFromUrlIfAvailable("a:#help");
+    await terminal_mutex.lock();
+    prefix.innerText = "[guest@jorn.works:~]$ ";
+    terminal_mutex.unlock();
+    performActionFromUrlIfAvailable(window.location.href);
+  });
+})();
